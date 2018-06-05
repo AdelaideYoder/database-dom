77264784210ce72cbf7f933ef59cc6f281844fb2
Adelaide Yoder <adelaide.yoder@gmail.com>
2018-06-05 17:14:01 -0500

    added a function to display my inventory on the dom, then added the advanced challenge, then added in the long version of Jisie's .foreach loop

--- database.js
+++ database.js
@@ -1,3 +1,5 @@
+// In your JavaScript file, load your home inventory from local storage.
+
 const loadDatabase = function (localStorageKey) {
     // Get the string version of the database
     const databaseString = localStorage.getItem(localStorageKey)
@@ -7,6 +9,83 @@ const loadDatabase = function (localStorageKey) {
 }
 
 // pull database back from local storage
-let myStuff = loadDatabase("HomeInventory");
+let allTheStuff = loadDatabase("HomeInventory");
 
-console.log(myStuff);
+console.log("This is to see the inventory database", allTheStuff);
+
+//______________________________THE NEXT PART_______________________________________
+// Iterate over every data set in the database and create a <section> component.
+
+const HomeInventoryDatabase = loadDatabase("HomeInventory")
+console.log("this is it", HomeInventoryDatabase)
+
+const myStuffArticle = document.querySelector("#myStuff")
+
+const homeInventoryFragment = document.createDocumentFragment()
+
+//---------------------------FIRST WAY TO WRITE FUNCTION USING .forEach--------------------------
+// for (let tableForm in HomeInventoryDatabase) {
+//     const invTable = HomeInventoryDatabase[tableForm]
+//     invTable.forEach(item => {
+//       const sectionElement = document.createElement("section")
+//       for (let key in item) {
+//         const paragraphElement = document.createElement("p")
+//         paragraphElement.textContent = `${key}: ${item[key]}`
+//         sectionElement.appendChild(paragraphElement)
+//       }
+//       homeInventoryFragment.appendChild(sectionElement)
+//     })
+//   }  
+  
+
+//   myStuffArticle.appendChild(homeInventoryFragment)
+
+//-------------------------------SECOND WAY TO WRITE FUNCTION USING LONG WAY-----------------------
+//_______________"FOR IN" LOOP_________"FOR" LOOP_________"FOR IN" LOOP___________________________
+for (let tableForm in HomeInventoryDatabase) {
+    const invTable = HomeInventoryDatabase[tableForm]
+    // invTable.forEach(item => {
+        console.log("invTable", invTable)
+    for (let i = 0; i < invTable.length; i++) {
+      const sectionElement = document.createElement("section")
+      for (let key in invTable[i]) {
+        const paragraphElement = document.createElement("p")
+        paragraphElement.textContent = `${key}: ${invTable[i][key]}`
+        sectionElement.appendChild(paragraphElement)
+      }
+      homeInventoryFragment.appendChild(sectionElement)
+    }
+  }  
+  
+
+  myStuffArticle.appendChild(homeInventoryFragment)
+
+// That component itself should contain three <p> components. One for the name, location, and description.
+// Attach the p components as children of the section.
+// Attach the section as a child of the article.
+
+
+//___________________________ADVANCED CHALLENGE_____________________________________
+
+// const HomeInventoryDatabase = loadDatabase("HomeInventory")
+
+// const myStuffArticle = document.querySelector("#myStuff")
+
+// const homeInventoryFragment = document.createDocumentFragment()
+
+// const populateDomForDataSet = (dataSet) => {
+//     const invTable = HomeInventoryDatabase[dataSet]
+//     invTable.forEach(item => {
+//       const sectionElement = document.createElement("section")
+//       for (let key in item) {
+//         const paragraphElement = document.createElement("p")
+//         paragraphElement.textContent = `${key}: ${item[key]}`
+//         sectionElement.appendChild(paragraphElement)
+//       }
+//       homeInventoryFragment.appendChild(sectionElement)
+//     }) 
+//   }
+  
+//   populateDomForDataSet("movies")
+  
+//   myStuffArticle.appendChild(homeInventoryFragment)
